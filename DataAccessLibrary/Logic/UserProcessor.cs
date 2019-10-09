45ed39f5d678137b040ecee3cf81a960abb4b499
s033224 <s033224@ad.viko.lt>
2019-10-09 21:44:45 +0300

    Removed password from sending sharing api

--- DataAccessLibrary/Logic/UserProcessor.cs
+++ DataAccessLibrary/Logic/UserProcessor.cs
@@ -19,14 +19,14 @@ namespace DataAccessLibrary.Logic
         
         public static List<UserModel> GetUsers()
         {
-            var sql = @"select * from dbo.Account;";
+            var sql = @"select FirstName, LastName, Email, Username, Icon from dbo.Account;";
 
             return DataAccess.DataAccess.LoadData<UserModel>(sql);
         }
 
         public static UserModel GetUser(int id)
         {
-            var sql = $"select * from dbo.Account where Id = {id};";
+            var sql = $"select FirstName, LastName, Email, Username, Icon from dbo.Account where Id = {id};";
             return DataAccess.DataAccess.GetSingleData<UserModel>(sql);
         }
 
