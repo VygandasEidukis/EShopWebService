ba622f07f0d9422e09d5de9ddbc1cb0dd6caa55f
s033224 <s033224@ad.viko.lt>
2019-10-11 12:22:20 +0300

    created models for product and image

---
+++ DataAccessLibrary/Models/ImageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Models
+{
+    class ImageModel
+    {
+        public int? Id { get; set; }
+        public byte[] ImageBytes { get; set; }
+        public int? ProductID { get; set; }
+    }
+}
